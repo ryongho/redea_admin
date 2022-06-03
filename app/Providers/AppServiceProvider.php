700d037ec374707b98e4341e2abe8f10de7d1c12
yongho0720 <roh.yh0720@gmail.com>
2022-06-03 16:02:54 +0900

    페이지 추가

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -24,7 +24,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        if(env('FORCE_HTTPS',false)) { // Default value should be false for local server
+        if(env('FORCE_HTTPS',true)) { // Default value should be false for local server
             URL::forceScheme('https');
         }
     }
