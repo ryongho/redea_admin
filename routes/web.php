ca09e133ff52005fe4702d0567969e90a1c0673d
yongho0720 <roh.yh0720@gmail.com>
2022-12-28 13:51:57 +0900

    회원 리스트 수정

--- routes/web.php
+++ routes/web.php
@@ -35,7 +35,7 @@ Route::post('/login_proc', [UserController::class, 'login'])->name('login_proc')
 Route::middleware('auth:sanctum')->get('/logout', [UserController::class, 'logout'])->name('logout');
 
 Route::get('/', function (Request $request) {
-    $list = UserController::get_list();
+    $list = UserController::get_list($request);
     //dd($list->data[0]->data);
     return view('user_list', ['list' => $list]);
 })->name('user_list');
