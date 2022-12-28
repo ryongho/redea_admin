62c499e3cded0d1c3a7064cc17ae0ff4e3456665
yongho0720 <roh.yh0720@gmail.com>
2022-12-28 13:49:18 +0900

    회원 리스트 수정

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -17,7 +17,7 @@ class UserController extends Controller
         return view('regist_user');
     }
 
-    public static function get_list(){
+    public static function get_list(Request $request){
         $row = 30;
 
         $page_no = 1;
