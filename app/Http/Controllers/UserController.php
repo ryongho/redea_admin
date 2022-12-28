a6c79037777dbf6ee35fc49f67ec781a62a46f1b
yongho0720 <roh.yh0720@gmail.com>
2022-12-28 14:05:21 +0900

    회원 리스트 수정

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -46,7 +46,7 @@ class UserController extends Controller
         
     }
 
-    public static function get_wait_list(){
+    public static function get_wait_list(Request $request){
 
         $row = 30;
 
