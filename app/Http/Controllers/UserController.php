36f5af1546992c629d1170d65574e19c872e40ef
yongho0720 <roh.yh0720@gmail.com>
2023-01-16 22:16:55 +0900

    최종반영

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -78,7 +78,7 @@ class UserController extends Controller
                         return $query->where('name',"like", "%".$search_arr[1]."%");
                     }else{
                         $table_obj = DB::table('table_users')->select('table_idx')->where('user_idx',$search_arr[1])->get();
-                        $table_arr = new array();
+                        $table_arr = array();
                         foreach($table_obj as $table){
                             array_push($table_arr, $table->table_idx);
                         }
@@ -95,7 +95,7 @@ class UserController extends Controller
                         return $query->where('name',"like", "%".$search_arr[1]."%");
                     }else{
                         $table_obj = DB::table('table_users')->select('table_idx')->where('user_idx',$search_arr[1])->get();
-                        $table_arr = new array();
+                        $table_arr = array();
                         foreach($table_obj as $table){
                             array_push($table_arr, $table->table_idx);
                         }
